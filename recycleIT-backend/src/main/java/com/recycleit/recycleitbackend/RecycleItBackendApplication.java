b1c57ebf3c92d7bc13179f8bcaf92bce35146389
Zakhar Zakharchuk <zakhar.zakharchuk3@gmail.com>
2022-11-22 12:40:46 +0200

    backend init

---
+++ recycleIT-backend/src/main/java/com/recycleit/recycleitbackend/RecycleItBackendApplication.java
@@ -0,0 +1,13 @@
+package com.recycleit.recycleitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecycleItBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecycleItBackendApplication.class, args);
+    }
+
+}
