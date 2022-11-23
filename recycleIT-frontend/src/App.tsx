3ce44d411747b58a13d1d202178ed7fc4f655fb3
Mariia-Kovalenko <mariya.kovalenko.2000@gmail.com>
2022-11-23 21:54:47 +0200

    RT-SIDE_PANEL_1 remove ServicesList component rendering from App component

--- recycleIT-frontend/src/App.tsx
+++ recycleIT-frontend/src/App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from './Components/Services/Map/Map';
-import ServicesList from './Components/Services/ServicesList/ServicesList';
 function App() {
   return (
     <div className="App">
-      {/* <Map/> */}
-      <ServicesList/>
+      <Map/>
     </div>
   );
 }
