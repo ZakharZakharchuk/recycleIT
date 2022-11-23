a9b1d31adc3a3b3b1d145e50a24061f83e64204f
ani778 <ani778h@gmail.com>
2022-11-23 15:18:57 +0400

    Components

---
+++ recycleIT-frontend/src/Components/Button/Button.tsx
@@ -0,0 +1,6 @@
+const Button = () => {
+    return (
+        <div>button</div>
+    )
+}
+export default Button
---
+++ recycleIT-frontend/src/Components/Services/index.tsx
@@ -0,0 +1,12 @@
+import Map from './Map/Map'
+// import ServicesList from './ServicesList/ServicesList'
+
+const Services = () => {
+    return (
+        <>
+            {/* <Header/> */}
+            <Map/>
+            {/* <ServicesList/> */}
+        </>
+    )
+}
