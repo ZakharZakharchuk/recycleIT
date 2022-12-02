b042635c67d3baa1259e1a0fb74c617e7128ebc3
Zakhar Zakharchuk <zakhar.zakharchuk3@gmail.com>
2022-12-02 15:46:21 +0200

    RT-registration and login

---
+++ recycleIT-backend/src/main/java/com/recycleit/recycleitbackend/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.recycleit.recycleitbackend.config;
+
+import com.recycleit.recycleitbackend.security.jwt.JwtConfigurer;
+import com.recycleit.recycleitbackend.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private static final String ADMIN_ENDPOINT = "/facilities/subtypes";
+    private static final String LOGIN_ENDPOINT = "/login";
+
+    @Autowired
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .httpBasic().disable()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .antMatchers(LOGIN_ENDPOINT).permitAll()
+            .antMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
+/*            .anyRequest().authenticated()*/
+            .and()
+            .apply(new JwtConfigurer(jwtTokenProvider));
+    }
+}
---
+++ recycleIT-backend/src/main/java/com/recycleit/recycleitbackend/dto/UserDto.java
@@ -0,0 +1,53 @@
+package com.recycleit.recycleitbackend.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.recycleit.recycleitbackend.entity.User;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserDto {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User mapToUser(){
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(password);
+        return user;
+    }
+}
