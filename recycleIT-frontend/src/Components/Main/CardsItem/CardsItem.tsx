22cdca10f30436c9d4e9d6aec99d0ca84bc16379
Mariia-Kovalenko <mariya.kovalenko.2000@gmail.com>
2022-12-05 13:54:51 +0200

    improved items styling, added services types section

--- recycleIT-frontend/src/Components/Main/CardsItem/CardsItem.tsx
+++ recycleIT-frontend/src/Components/Main/CardsItem/CardsItem.tsx
@@ -2,34 +2,42 @@ import { TabPanel } from "@mui/lab"
 import { Card, CardActionArea, CardMedia, Typography } from "@mui/material"
 import styles from '../Main.module.css'
 
-const CardsItem = ({setItem, item, ButtonData}:any) => {
+interface ICardItemProps {
+    id: string, 
+    title: string, 
+    category: string, 
+    desc: string, 
+    image: string, 
+    value:string 
+}
+
+const CardsItem = ({item}: {item: ICardItemProps[]}) => {
     return (
-        <>
+        <div className={styles.Cards_container} >
             {
-                item.map((card: {title: string,category: string, desc: string, image: string, value:string}, id: number) => {
+                item.map((card: ICardItemProps) => {
                     return (
-                        <TabPanel value={card.value}>
-                            <Card className={styles.Card_wrapper} key={id}>
-                                <CardActionArea className={styles.Card_inside_wrapper}> 
-                                    <Typography variant="h6" className={styles.Card_typography}>
+                            <Card className={styles.Card_wrapper} key={card.id}>
+                                <div className={styles.Card_inside_wrapper}> 
+                                    <Typography gutterBottom variant="h6" className={styles.Card_typography}>
                                         {card.title}
                                     </Typography>
-                                    <Typography variant="body2" className={styles.Card_parg} >
+                                    <Typography gutterBottom variant="body2" className={styles.Card_parg} >
                                         {card.desc}
                                     </Typography>
                                     <CardMedia
                                         className={styles.Card_img}
                                         component="img"
-                                        src={card.image}
+                                        image={card.image}
                                         alt="img"
+                                        height="200"
                                     />                                 
-                            </CardActionArea>
-                        </Card>
-                    </TabPanel>
+                                </div>
+                            </Card>
                     )
                 })
             }
-        </>
+        </div>
     )
 }
 
