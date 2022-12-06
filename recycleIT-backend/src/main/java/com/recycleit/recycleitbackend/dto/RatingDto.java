ec880c743d6b6ff799f36c7f19dcb35da1ca08aa
Ivan <shtrikh14@gmail.com>
2022-12-07 01:10:04 +0200

    Rating

---
+++ recycleIT-backend/src/main/java/com/recycleit/recycleitbackend/dto/RatingDto.java
@@ -0,0 +1,27 @@
+package com.recycleit.recycleitbackend.dto;
+
+import com.recycleit.recycleitbackend.entity.Rating;
+import com.recycleit.recycleitbackend.entity.User;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RatingDto {
+    private Long id;
+    private User userId;
+    private Long facilityId;
+    private Integer mark;
+
+    public Rating mapToRating(){
+        return Rating.builder()
+                .id(id)
+                .userId(userId)
+                .facilityId(facilityId)
+                .mark(mark)
+                .build();
+    }
+
+}
---
+++ recycleIT-backend/src/main/java/com/recycleit/recycleitbackend/service/impl/RatingServiceImpl.java
@@ -0,0 +1,41 @@
+package com.recycleit.recycleitbackend.service.impl;
+
+import com.recycleit.recycleitbackend.dto.RatingDto;
+import com.recycleit.recycleitbackend.entity.Rating;
+import com.recycleit.recycleitbackend.repository.RatingRepository;
+import com.recycleit.recycleitbackend.service.RatingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RatingServiceImpl implements RatingService {
+
+    private final RatingRepository ratingRepository;
+
+    @Override
+    public void setRating(RatingDto rating) {
+        ratingRepository.save(rating.mapToRating());
+    }
+
+    @Override
+    public void changeRating(Long serviceId, Long userId, int mark) {
+        ratingRepository.changeRating(userId, serviceId, mark);
+    }
+
+    @Override
+    public Integer getServiceRating(Long serviceId) {
+
+        List<Rating> allMarks= ratingRepository.getRatingsByFacilityId(serviceId);
+        double avgMarks = 0;
+
+        for (Rating mark:allMarks) {
+            avgMarks += mark.getMark().doubleValue();
+        }
+
+        avgMarks = avgMarks/allMarks.size();
+        return (int) avgMarks;
+    }
+}
