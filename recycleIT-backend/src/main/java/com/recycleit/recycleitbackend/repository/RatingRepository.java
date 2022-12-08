65e91f8b02afd6618dffa2e59d7ce7094bfa63c3
Zakhar Zakharchuk <zakhar.zakharchuk3@gmail.com>
2022-12-08 18:19:33 +0200

    Rt-delete unused imports and exceptions

--- recycleIT-backend/src/main/java/com/recycleit/recycleitbackend/repository/RatingRepository.java
+++ recycleIT-backend/src/main/java/com/recycleit/recycleitbackend/repository/RatingRepository.java
@@ -2,13 +2,11 @@ package com.recycleit.recycleitbackend.repository;
 
 import com.recycleit.recycleitbackend.entity.Rating;
 import com.recycleit.recycleitbackend.entity.User;
+import java.math.BigDecimal;
+import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
-import java.math.BigDecimal;
 
 @Repository
 @Transactional
--- recycleIT-backend/src/main/java/com/recycleit/recycleitbackend/security/jwt/JwtConfigurer.java
+++ recycleIT-backend/src/main/java/com/recycleit/recycleitbackend/security/jwt/JwtConfigurer.java
@@ -8,14 +8,14 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class JwtConfigurer extends
     SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
-    private JwtTokenProvider jwtTokenProvider;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public JwtConfigurer(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Override
-    public void configure(HttpSecurity httpSecurity) throws Exception {
+    public void configure(HttpSecurity httpSecurity) {
         JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
         httpSecurity.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
     }
