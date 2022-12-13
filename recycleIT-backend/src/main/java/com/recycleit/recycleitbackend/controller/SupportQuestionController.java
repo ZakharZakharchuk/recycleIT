40b5a705629a28bf737b389ffe9a3c93e7c8f2fc
Zakhar Zakharchuk <zakhar.zakharchuk3@gmail.com>
2022-12-13 16:43:35 +0200

    Validation and delivery

--- recycleIT-backend/src/main/java/com/recycleit/recycleitbackend/controller/SupportQuestionController.java
+++ recycleIT-backend/src/main/java/com/recycleit/recycleitbackend/controller/SupportQuestionController.java
@@ -3,6 +3,7 @@ package com.recycleit.recycleitbackend.controller;
 import com.recycleit.recycleitbackend.dto.QuestionRequestDto;
 import com.recycleit.recycleitbackend.service.SupportQuestionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ public class SupportQuestionController {
     private final SupportQuestionService supportQuestionService;
 
     @PostMapping("/send")
-    void sendQuestion(@RequestBody QuestionRequestDto questionRequestDto) {
+    void sendQuestion(@Validated @RequestBody QuestionRequestDto questionRequestDto) {
         supportQuestionService.sendQuestion(questionRequestDto);
     }
 }
--- recycleIT-backend/src/main/java/com/recycleit/recycleitbackend/service/impl/ExternalAPIServiceImpl.java
+++ recycleIT-backend/src/main/java/com/recycleit/recycleitbackend/service/impl/ExternalAPIServiceImpl.java
@@ -10,6 +10,7 @@ import com.recycleit.recycleitbackend.entity.FacilitySubtype;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 import com.recycleit.recycleitbackend.service.ExternalAPIService;
@@ -24,38 +25,40 @@ public class ExternalAPIServiceImpl implements ExternalAPIService {
 
     private static final String SUBTYPES_REQUEST_URL = "https://iwaste.epa.gov/api/disposal-facility-subtypes";
     private final static List<Long> SUBTYPES_ID = Arrays.asList(1L, 4L, 6L, 11L, 34L, 36L, 37L, 38L,
-            39L, 40L);
+        39L, 40L);
     private static final String FACILITIES_REQUEST_URL =
-            "https://iwaste.epa.gov/api/facilities?facilityTypeId=%s&stateCode=%s&epaRegion=";
+        "https://iwaste.epa.gov/api/facilities?facilityTypeId=%s&stateCode=%s&epaRegion=";
     private static final String FACILITIES_ARRAY_NAME = "data";
     private final RestTemplate restTemplate = new RestTemplate();
 
     @Override
     public List<FacilitySubtype> getSubtypes() {
         FacilitySubtype[] objects = restTemplate
-                .getForEntity(SUBTYPES_REQUEST_URL, FacilitySubtype[].class)
-                .getBody();
+            .getForEntity(SUBTYPES_REQUEST_URL, FacilitySubtype[].class)
+            .getBody();
         return Arrays.stream(objects)
-                .filter(subtype -> SUBTYPES_ID.contains(subtype.getId()))
-                .collect(Collectors.toList());
+            .filter(subtype -> SUBTYPES_ID.contains(subtype.getId()))
+            .collect(Collectors.toList());
     }
 
     @Override
     public List<Facility> getFacilities(FacilityRequest facilityRequest)
-            throws JsonProcessingException {
+        throws JsonProcessingException {
         String url = String.format(FACILITIES_REQUEST_URL, facilityRequest.getFacilityTypeId(),
-                facilityRequest.getStateCode());
+            facilityRequest.getStateCode());
 
         ResponseEntity<Object> response = restTemplate.getForEntity(url,
-                Object.class);
-        System.out.println(restTemplate.getForEntity(url, Facility.class));
+            Object.class);
         String json = new Gson().toJson(response.getBody());
         JSONObject jsonObject = new JSONObject(json);
         JSONArray data = jsonObject.getJSONArray(FACILITIES_ARRAY_NAME);
 
-        System.out.println(data);
         ObjectMapper mapper = new ObjectMapper();
         Facility[] facilities = mapper.readValue(data.toString(), Facility[].class);
-        return Arrays.stream(facilities).collect(Collectors.toList());
+        Random random = new Random();
+        List<Facility> facilityList = Arrays.stream(facilities)
+            .collect(Collectors.toList());
+        facilityList.forEach(facility -> facility.setDelivery(random.nextBoolean()));
+        return facilityList;
     }
 }
